027a05c4abbabe7dc7dbf77083b1f3f8f9b06772
Van Le <singularity@vannio.co.uk>
2016-07-12 15:56:47 +0100

    Scaffold user model

--- db/migrate/20160712143356_create_users.rb
+++ db/migrate/20160712143356_create_users.rb
@@ -5,7 +5,6 @@ class CreateUsers < ActiveRecord::Migration
       t.string :last_name
       t.string :email
       t.string :password_digest
-      t.date :created_at
 
       t.timestamps null: false
     end
