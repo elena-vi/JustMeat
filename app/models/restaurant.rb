7cc265bbe8e69f7dc9dda0de79a601c7ad63ea11
Alex-Swann <alex_swann@hotmail.co.uk>
2016-07-13 17:35:49 +0100

    added review associations with users and restaurants

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,4 +1,5 @@
 class Restaurant < ActiveRecord::Base
   belongs_to :user
+  has_many :reviews
   validates_associated :user
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160713133937) do
+ActiveRecord::Schema.define(version: 20160713162936) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,17 @@ ActiveRecord::Schema.define(version: 20160713133937) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.integer  "user_id"
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.text     "description"
+    t.string   "image"
+    t.string   "phone_number"
+    t.string   "address"
+    t.integer  "user_id"
+    t.integer  "restaurant_id"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
   end
 
   create_table "users", force: :cascade do |t|
