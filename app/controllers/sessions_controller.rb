648a3ac68b44aa30462c4978befb5486bb09de28
Elena <elena.vilimaite@gmail.com>
2016-07-14 10:47:38 +0100

    renamed password_digest to password

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -26,6 +26,6 @@ private
 
     # Never trust parameters from the scary internet, only allow the white list through.
   def user_params
-    params.require(:user).permit(:email, :password_digest)
+    params.require(:user).permit(:email, :password)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,10 @@
 class User < ActiveRecord::Base
-  has_many :restaurants
-  has_many :reviews
+	has_many :restaurants
+	has_many :reviews
+
+	attr_accessor :password
+	EMAIL_REGEX = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i
+	validates :email, :presence => true, :uniqueness => true, :format => EMAIL_REGEX
+	validates :password, :confirmation => true #password_confirmation attr
+	validates_length_of :password, :in => 6..20, :on => :create
 end
